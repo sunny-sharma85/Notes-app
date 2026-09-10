be5a9cfcde3d1553d57b5cdee5c03a06bc2831cb
Sunny Sharma <shandilsharma1010@gmail.com>
2026-09-10 17:47:45 +0530

    localstorageadded

--- src/components/NotesApp.jsx
+++ src/components/NotesApp.jsx
@@ -2,15 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 
 const NotesApp = () => {
-  const header ={"Acess-Control-Allow-Origin": "*"};
 
   const[notes,SetNotes]=useState({
     title:"",
     description:""
   })
   const[error,setError]=useState("")
-  const[getNotes,SetGetNotes]=useState([])
-  const[editId,setEditId]=useState(null)
+  const[getNotes,SetGetNotes]=useState(
+     JSON.parse(localStorage.getItem("notes")) || [] )
+
+  
+  const[editId,Seteditid]=useState(null)
   const handlechange=(e)=>{
 SetNotes({
   ...notes,
@@ -25,49 +27,51 @@ SetNotes({
       return;
     }
     if(editId){
-      await axios.put(`https://6a853c749c451dc67a636590.mockapi.io/Notes/${editId}`)
+      const updatenotes=getNotes.map((item)=> item.id===editId? {...item,...notes}:item);
+      SetGetNotes(updatenotes)
+      localStorage.setItem("notes",JSON.stringify(updatenotes))
+      Seteditid(null)
+      SetNotes({
+        title:"",
+        description:""
+      })
     }
     else{
-      axios.post("https://6a853c749c451dc67a636590.mockapi.io/Notes",
-        notes,{
-          headers:{"Content-Type": "application/json"}
-        }
-      )
       
+      
+     
+     const NewNotes= {
+      id:Date.now(),
+      ...notes,
+
+     }
+     const updatednotes=[...getNotes,NewNotes]
+     SetGetNotes(updatednotes)
+     
+      localStorage.setItem("notes",JSON.stringify(updatednotes))
       SetNotes({
         title:"",
         description:""
       })
       setError("")
     
-    
+    }
   }
-}
-const fetchNotes= async()=>{
-  try{
-  const res= await axios.get("https://6a853c749c451dc67a636590.mockapi.io/Notes")
-  SetGetNotes(res.data)
-  }
-  catch(err){
-    console.log("Error")
-  }
-}
+
+
 const handleedit=(note)=>{
   SetNotes({
     title: note.title,
     description: note.description
   });
-setEditId(note.id)
+Seteditid(note.id)
 }
-const handledelete= async(id)=>{
-   await axios.delete(`https://6a853c749c451dc67a636590.mockapi.io/Notes/${id}`)
-  SetGetNotes((note)=> note.filter((item)=> item.id!==id))
+const handledelete= (id)=>{
+   const updatenotes=getNotes.filter((note)=> note.id !== id);
+   SetGetNotes(updatenotes)
+   localStorage.setItem("notes",JSON.stringify(updatenotes))
 };
-useEffect(()=>{
-  fetchNotes();
-  
 
-},[])
   return (
      <main className="notes-app">
 
@@ -95,7 +99,7 @@ useEffect(()=>{
             onChange={handlechange}
           ></textarea>
 {error&& <p style={{color:"red"}}>{error}</p>}
-          <button>Add Note</button>
+          <button>{editId? "Update": "AddNote"}</button>
 
         </form>
 
